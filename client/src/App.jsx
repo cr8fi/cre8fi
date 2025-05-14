ff8c7e17e193fda490f18dadecdbe6186b06e35c
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-05-14 15:13:12 +0100

    modified: new routes for Dashboard created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import Register from './pages/auth/Register';
-import PasswordReset from './pages/auth/PasswordReset';
-import PasswordResetCode from './pages/auth/PasswordResetCode';
-import SuccessScreen from './components/SuccessScreen';
-import NewPassword from './pages/auth/NewPassword';
-import Login from './pages/auth/Login';
+import { Routes, Route, Link } from "react-router-dom";
+import Register from "./pages/auth/Register";
+import PasswordReset from "./pages/auth/PasswordReset";
+import PasswordResetCode from "./pages/auth/PasswordResetCode";
+import SuccessScreen from "./components/SuccessScreen";
+import NewPassword from "./pages/auth/NewPassword";
+import Login from "./pages/auth/Login";
 import HomePage from "./pages/HomePage";
 import Home from "./pages/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
-  
-      
-      <Routes>
-        
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset" element={<PasswordReset />} />
-        <Route path="/code" element={<PasswordResetCode />} />
-        <Route path="/success" element={<SuccessScreen />} />
-        <Route path="/new" element={<NewPassword />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<Home />} />
-      
-      </Routes>
-  )}
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/reset" element={<PasswordReset />} />
+      <Route path="/code" element={<PasswordResetCode />} />
+      <Route path="/success" element={<SuccessScreen />} />
+      <Route path="/new" element={<NewPassword />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
 
-export default App
+export default App;
