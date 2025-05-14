6ed41ede1c9c72a7ad28175633dd662cf4f7a76c
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-05-14 19:34:16 +0100

    add: all form sections are here

---
+++ client/src/pages/dashboard/UI/Form4.jsx
@@ -0,0 +1,31 @@
+import Button from "../Components/Button";
+import Header from "../Components/Header";
+import Input from "../Components/Input";
+
+export default function Form4() {
+  return (
+    <form action="#" className="py-10 px-20 flex flex-col gap-10">
+      <Header text="Date of Birth" />
+
+      <fieldset className="flex flex-col gap-6">
+        <Input
+          type="radio"
+          name="how"
+          id="creator"
+          defaultChecked
+          heading="Creator"
+          paragraph="I want to showcase my skills and work on projects"
+        />
+        <Input
+          type="radio"
+          name="how"
+          id="project"
+          heading="Project"
+          paragraph="I need to find creative talents and manage my projects"
+        />
+      </fieldset>
+
+      <Button />
+    </form>
+  );
+}
