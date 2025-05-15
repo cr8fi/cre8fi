df6b7e223022aaf73d3f773a119dbe696c9a1fd7
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-05-15 15:51:46 +0100

    contomized this section separately

--- client/src/pages/dashboard/UI/Form4.jsx
+++ client/src/pages/dashboard/UI/Form4.jsx
@@ -1,27 +1,37 @@
-import Button from "../Components/Button";
 import Header from "../Components/Header";
-import Input from "../Components/Input";
 
 export default function Form4() {
   return (
     <>
       <Header text="Date of Birth" />
 
-      <fieldset className="flex flex-col gap-6">
-        <Input
-          type="radio"
-          name="how"
-          id="creator"
-          defaultChecked
-          heading="Creator"
-          paragraph="I want to showcase my skills and work on projects"
+      <fieldset className="flex gap-6">
+        <input
+          type="number"
+          name="day"
+          id="day"
+          placeholder="Day"
+          min={1}
+          max={31}
+          className="peer w-full bg-transparent p-3 rounded-md border outline-none border-[#4565DB] focus:border-[#9C39FF]"
         />
-        <Input
-          type="radio"
-          name="how"
-          id="project"
-          heading="Project"
-          paragraph="I need to find creative talents and manage my projects"
+        <input
+          type="number"
+          name="month"
+          id="month"
+          placeholder="Month"
+          min={1}
+          max={12}
+          className="peer w-full bg-transparent p-3 rounded-md border outline-none border-[#4565DB] focus:border-[#9C39FF]"
+        />
+        <input
+          type="number"
+          name="year"
+          id="year"
+          placeholder="Year"
+          min={1900}
+          max={2100}
+          className="peer w-full bg-transparent p-3 rounded-md border outline-none border-[#4565DB] focus:border-[#9C39FF]"
         />
       </fieldset>
     </>
