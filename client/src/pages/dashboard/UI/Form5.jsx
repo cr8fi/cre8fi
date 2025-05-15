f69ee8c8d7a2a2674b4a9f05f9dad0ab0c19e3a8
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-05-15 15:50:16 +0100

    new form section implemented

---
+++ client/src/pages/dashboard/UI/Form5.jsx
@@ -0,0 +1,22 @@
+import Header from "../Components/Header";
+
+export default function Form5() {
+  return (
+    <>
+      <Header text="Tell us a bit about yourself" />
+      <fieldset>
+        <header>
+          <h2 className="text-xl font-semibold mb-4">
+            Write a short description about you
+          </h2>
+        </header>
+        <textarea
+          name="description"
+          id="description"
+          placeholder="I am a graphics designer who solve all your problem with my ............."
+          className="w-full bg-transparent p-5 rounded-md border outline-none border-[#4565DB] focus:border-[#9C39FF] resize-none h-44"
+        ></textarea>
+      </fieldset>
+    </>
+  );
+}
