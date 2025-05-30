ceb41e0c8e2a15f278ea0d616281f07549f3890d
dantel <rowlanddantel@gmail.com>
2025-05-30 14:31:32 -0800

    email testing

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
-from utils.email import  send_verification_email
+from accounts.utils.email import  send_verification_email
 import jwt
 from django.conf import settings
 
