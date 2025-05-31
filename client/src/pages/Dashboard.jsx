d7e68ac9803ac4c4af25961481cd114e3a25bf4a
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-05-31 20:47:13 +0100

    Modified dashboard view

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
-import Feed from "./auth/UI/Feeds";
+import Feeds from "./auth/UI/Feeds";
 import RightPanel from "./auth/UI/RightPanel";
 import Sidebar from "./auth/UI/Sidebar";
 
 export default function Dashboard() {
   return (
-    <main className="bg-[#030214] flex min-h-screen min-w-max">
+    <main className="bg-[#030214] flex min-h-screen min-w-[1024px]">
       <Sidebar />
       {/* Main content */}
-      <Feed />
+      <Feeds />
       {/* Right Panel Content */}
       <RightPanel />
     </main>
